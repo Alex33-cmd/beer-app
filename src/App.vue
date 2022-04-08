1f9163ea4232b8e509c90d330adf83ffe470ab9b
Alex33-cmd <62179831+Alex33-cmd@users.noreply.github.com>
2022-04-08 18:18:21 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import HelloBeer from './components/HelloBeer.vue'
+</script>
+
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloBeer msg="Beer App v0.1" />
+</template>
+
+<style>
+html {
+  background: #000;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #fff700;
+  margin-top: 60px;
+}
+</style>
